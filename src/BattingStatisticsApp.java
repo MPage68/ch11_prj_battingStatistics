57a0587707a39e75812c43b5bce1001d68dc0fb4
mpage68 <mikepage68@live.com>
2018-09-20 20:39:01 -0400

    update

--- src/BattingStatisticsApp.java
+++ src/BattingStatisticsApp.java
@@ -8,37 +8,44 @@ public class BattingStatisticsApp {
 		Scanner sc = new Scanner(System.in);
 
 		String choice = "y";
-		double slugging = 0.0;
-		double battingAvg = 0.0;
+		double slugging = 0;
+		int sluggingTotal = 0;
+		double battingAvg = 0;
 		int size = 0;
 		int result = 0;
 
 		System.out.println("Welcome to the batting average calculator");
-		System.out.println();
-		System.out.print("Enter number of times at bat:  ");
+		System.out.println("");
+		System.out.print("Enter number of times at bat:  ");		
 		size = sc.nextInt();
+		System.out.println();
 		System.out.println("0 = out, 1 = single, 2 = double, 3 = triple, 4 = home run");
-
+		System.out.println();
 		while (choice.equalsIgnoreCase("y")) {
 
 			int[] atBats = new int[size];
 			for (int i = 0; i < size; i++) {
-				// atBats[i] = i;
 
 				System.out.print("Result for at-bat: ");
 				atBats[i] = sc.nextInt();
-				result += atBats[i];
+				sluggingTotal += atBats[i];
+				if (atBats[i] > 0) {
+					result++;
+				}
 
-				battingAvg = result / size;
-				battingAvg = (double) Math.round(battingAvg * 100) / 100;
-				
+				battingAvg = (double) result / size;
+				battingAvg = (double) Math.round(battingAvg * 1000) / 1000;
+
+				slugging = (double) sluggingTotal / size;
+				slugging = (double) Math.round(slugging * 1000) / 1000;
 
 			}
-
+			System.out.println();
 			System.out.println("Batting average: " + battingAvg);
-
+			System.out.println("Slugging percent: " + slugging);
+			System.out.println();
+			choice = Console.getString("Another player? (y/n)  ", "y", "n");
 		}
-		// choice = Console.getString("Another player? (y/n)", "y", "n");
 
 		System.out.println("Bye");
 
--- src/Console.java
+++ src/Console.java
@@ -3,22 +3,22 @@
 import java.util.Scanner;
 
 public class Console {
-	
-	
 
 	private static Scanner sc = new Scanner(System.in);
+
 //y/n error handling 
 	public static String getString(String prompt) {
 		boolean isValid = false;
 		String s = "";
 		while (!isValid) {
 			System.out.print(prompt);
-		}
-		s = sc.nextLine();
-		if (s.equals("")) {
-			System.out.println("Error, This entry is required");
-		} else {
-			isValid = true;
+
+			s = sc.nextLine();
+			if (s.equals("")) {
+				System.out.println("Error, This entry is required");
+			} else {
+				isValid = true;
+			}
 		}
 		return s;
 	}
@@ -37,7 +37,7 @@ public class Console {
 		}
 
 		return s;
-	}//end y/n error handling
+	}// end y/n error handling
 
 	public static int getInt(String prompt) {
 		int i = 0;
